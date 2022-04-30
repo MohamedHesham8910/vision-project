903403bcc6acb5ea8a335267828b09b829f146d7
Haldnia <mohamedhesham8910@gmail.com>
2022-04-30 07:56:04 +0200

    images are saved with different numbers

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 from tkinter import *
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
@@ -7,11 +8,14 @@ from milestone1 import *
 #GUI
 
 main = Tk()
+main.title('Milestone 1')
 main.geometry("500x300")
 
 def open_file():
     global dir
+    global imgdir
     image = filedialog.askopenfile(mode='r', filetypes=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif', '.tiff', '.tif', '.bmp'])])
+    imgdir = os.path.dirname(image.name)
     dir = image.name
 
 img = Label(main,text="Please Choose an Image")
@@ -27,19 +31,19 @@ l = Label(main,textvariable=var)
 var.set("Choose Type of Augmentation to Perform")
 l.place(relx=0.5, rely=0.4, anchor=N)
 
-resizing = Button(main, text="Resize", command= lambda: resize(dir))
+resizing = Button(main, text="Resize", command= lambda: resize(dir,imgdir))
 resizing.place(relx=0.1, rely=0.55, anchor=N)
 
-trans = Button(main, text="Translate", command= lambda: translate(dir))
+trans = Button(main, text="Translate", command= lambda: translate(dir,imgdir))
 trans.place(relx=0.3, rely=0.55, anchor=N)
 
-rotation = Button(main, text="Rotation", command= lambda: rotate(dir))
+rotation = Button(main, text="Rotation", command= lambda: rotate(dir,imgdir))
 rotation.place(relx=0.5, rely=0.55, anchor=N)
 
-flipping = Button(main, text="Flip", command= lambda: flip(dir))
+flipping = Button(main, text="Flip", command= lambda: flip(dir,imgdir))
 flipping.place(relx=0.7, rely=0.55, anchor=N)
 
-zooming = Button(main, text="Zoom", command= lambda: zoom(dir))
+zooming = Button(main, text="Zoom", command= lambda: zoom(dir,imgdir))
 zooming.place(relx=0.9, rely=0.55, anchor=N)
 
 
--- milestone1.py
+++ milestone1.py
@@ -1,11 +1,14 @@
+from cmath import inf
 import cv2
 import numpy as np
 import random
+import os
 
 ####################################################################
 #Functions for Augmentations
 
-def resize(dir):
+def resize(dir,path):
+    cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
@@ -21,21 +24,22 @@ def resize(dir):
     resizedImage = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC) 
     cv2.imshow("Resized Image", resizedImage)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    os.chdir(path)
+    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), resizedImage)
 
-def translate(dir):
+def translate(dir,path):
+    cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
     translationMatrix = np.float32([[1,0, random.randint(10,(w//3.3))],[0,1,random.randint(10,(h//2.67))]])
     movedImage = cv2.warpAffine(image, translationMatrix, (image.shape[1],image.shape[0]))
     cv2.imshow("Moved image", movedImage)
+    os.chdir(path)
+    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), movedImage)
 
-    cv2.waitKey(0)
+def rotate(dir,path):
     cv2.destroyAllWindows()
-
-def rotate(dir):
     imagePath = dir
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
@@ -44,36 +48,38 @@ def rotate(dir):
     rotationMatrix = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotatedImage = cv2.warpAffine(image, rotationMatrix, (image.shape[1],image.shape[0]))
     cv2.imshow("Rotated image", rotatedImage) 
+    os.chdir(path)
+    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), rotatedImage)
 
-    cv2.waitKey(0)
+def flip(dir,path):
     cv2.destroyAllWindows()
-
-def flip(dir):
     imagePath = dir
     image = cv2.imread(imagePath)
     choice = random.randint(1,3)
     if choice == 1:
-        flippedHorizontally = cv2.flip(image, 1)
-        cv2.imshow("Flipped Horizontally", flippedHorizontally)
-        cv2.waitKey(-1)
+        flipped = cv2.flip(image, 1)
+        cv2.imshow("Flipped Horizontally", flipped)
+        os.chdir(path)
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
     elif choice == 2:
-        flippedVertically = cv2.flip(image, 0) 
-        cv2.imshow("Flipped Vertically", flippedVertically) 
-        cv2.waitKey(-1)
+        flipped = cv2.flip(image, 0) 
+        cv2.imshow("Flipped Vertically", flipped) 
+        os.chdir(path)
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
     elif choice == 3:
-        flippedHV = cv2.flip(image, -1) 
-        cv2.imshow("Flipped H and V", flippedHV) 
-        cv2.waitKey(-1)
-
+        flipped = cv2.flip(image, -1) 
+        cv2.imshow("Flipped H and V", flipped) 
+        os.chdir(path)
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
+        
+def zoom(dir,path):
     cv2.destroyAllWindows()
-
-def zoom(dir):
     imagePath = dir
     image = cv2.imread(imagePath)
     h = image.shape[0]
     zoom_factor = random.uniform((1080/h),4)
     Zoomed = cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor)
     cv2.imshow("Zoomed", Zoomed) 
-    cv2.waitKey(-1)
+    os.chdir(path)
+    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), Zoomed)
 
-    cv2.destroyAllWindows()
