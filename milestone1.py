2a9f55ab0c784c9a5d0cef86effcee0ebda315d7
Haldnia <mohamedhesham8910@gmail.com>
2022-05-01 07:30:10 +0200

    added loop for iteratio

--- milestone1.py
+++ milestone1.py
@@ -2,6 +2,7 @@ from cmath import inf
 import cv2
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 import os
 
 ####################################################################
@@ -13,73 +14,81 @@ def resize(dir,path):
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
     aspect = w/h
-    
-    ratio = random.uniform(0.2,(1080/h))
-
-    height = int(ratio * h) 
-    width = int(height * aspect)
-
-    dimension = (width,height)
-
-    resizedImage = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC) 
-    cv2.imshow("Resized Image", resizedImage)
-
     os.chdir(path)
-    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), resizedImage)
+    for i in range(6):
+        ratio = random.uniform(0.2,(1080/h))
+
+        height = int(ratio * h) 
+        width = int(height * aspect)
+
+        dimension = (width,height)
+
+        resizedImage = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), resizedImage) 
+        cv2.imshow("Resized Image", resizedImage)
+        cv2.waitKey(500)
 
 def translate(dir,path):
     cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
-    translationMatrix = np.float32([[1,0, random.randint(10,(w//3.3))],[0,1,random.randint(10,(h//2.67))]])
-    movedImage = cv2.warpAffine(image, translationMatrix, (image.shape[1],image.shape[0]))
-    cv2.imshow("Moved image", movedImage)
     os.chdir(path)
-    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), movedImage)
-
+    for i in range(6):
+        translationMatrix = np.float32([[1,0, random.randint(10,(w//3.3))],[0,1,random.randint(10,(h//2.67))]])
+        movedImage = cv2.warpAffine(image, translationMatrix, (image.shape[1],image.shape[0]))
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), movedImage)
+        cv2.imshow("Moved image", movedImage)
+        cv2.waitKey(500)
+    
 def rotate(dir,path):
     cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
     center = (h//2, w//2)
-    angle = random.randint(-180,180)
-    rotationMatrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotatedImage = cv2.warpAffine(image, rotationMatrix, (image.shape[1],image.shape[0]))
-    cv2.imshow("Rotated image", rotatedImage) 
     os.chdir(path)
-    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), rotatedImage)
+    for i in range(6):
+        angle = random.randint(-180,180)
+        rotationMatrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotatedImage = cv2.warpAffine(image, rotationMatrix, (image.shape[1],image.shape[0]))
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), rotatedImage)
+        cv2.imshow("Rotated image", rotatedImage)
+        cv2.waitKey(500)
 
 def flip(dir,path):
-    cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
-    choice = random.randint(1,3)
-    if choice == 1:
-        flipped = cv2.flip(image, 1)
-        cv2.imshow("Flipped Horizontally", flipped)
-        os.chdir(path)
-        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
-    elif choice == 2:
-        flipped = cv2.flip(image, 0) 
-        cv2.imshow("Flipped Vertically", flipped) 
-        os.chdir(path)
-        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
-    elif choice == 3:
-        flipped = cv2.flip(image, -1) 
-        cv2.imshow("Flipped H and V", flipped) 
-        os.chdir(path)
-        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
+    os.chdir(path)
+    for i in range(6):
+        cv2.destroyAllWindows()
+        choice = random.randint(1,3)
+        if choice == 1:
+            flipped = cv2.flip(image, 1)
+            cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
+            cv2.imshow("Flipped Horizontally", flipped)
+        elif choice == 2:
+            flipped = cv2.flip(image, 0) 
+            cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
+            cv2.imshow("Flipped Vertically", flipped) 
+        elif choice == 3:
+            flipped = cv2.flip(image, -1) 
+            cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
+            cv2.imshow("Flipped H and V", flipped) 
+            
+        cv2.waitKey(500)
         
 def zoom(dir,path):
     cv2.destroyAllWindows()
     imagePath = dir
     image = cv2.imread(imagePath)
     h = image.shape[0]
-    zoom_factor = random.uniform((1080/h),4)
-    Zoomed = cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor)
-    cv2.imshow("Zoomed", Zoomed) 
     os.chdir(path)
-    cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), Zoomed)
+    for i in range(6):
+        zoom_factor = random.uniform((1080/h),4)
+        Zoomed = cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor)
+        cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), Zoomed)
+        cv2.imshow("Zoomed", Zoomed)
+        cv2.waitKey(500)
+        
 
