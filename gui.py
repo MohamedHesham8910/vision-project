237a8b74d4e206888dd9ae2555f56bfac44b8053
Haldnia <mohamedhesham8910@gmail.com>
2022-05-10 19:16:45 +0200

    added Numbers of Iterations

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ from milestone1 import *
 
 main = Tk()
 main.title('Milestone 1')
-main.geometry("500x300")
+main.geometry("500x400")
 
 def open_file():
     global dir
@@ -17,6 +17,10 @@ def open_file():
     image = filedialog.askopenfile(mode='r', filetypes=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif', '.tiff', '.tif', '.bmp', 'svg'])])
     imgdir = os.path.dirname(image.name)
     dir = image.name
+
+def enterNo():
+    global num
+    num = e.get()
 
 img = Label(main,text="Choose an Image", font="Times 15 bold")
 
@@ -26,23 +30,32 @@ browse = Button(main, text="Browse",font="Times 12",bd= 3,command=open_file)
 
 browse.place(relx=0.5,rely=0.25,anchor=N)
 
+e = Entry(main)
+e.place(relx=0.3, rely=0.55, anchor=N)
+
+no = Label(main,text="Enter Number of Modified Data to Generate",font="Times 15 bold")
+no.place(relx=0.5, rely=0.4, anchor=N)
+
+enter = Button(main, text="Enter",font="Times 10",bd= 3,command=enterNo)
+enter.place(relx=0.5,rely=0.54,anchor=N)
+
 l = Label(main,text="Choose Type of Augmentation to Perform",font="Times 15 bold")
-l.place(relx=0.5, rely=0.45, anchor=N)
+l.place(relx=0.5, rely=0.65, anchor=N)
 
-resizing = Button(main, text="Resize",font="Times 12",bd= 3, command= lambda: resize(dir,imgdir))
-resizing.place(relx=0.3, rely=0.6, anchor=N)
+rotation = Button(main, text="Rotation",font="Times 12",bd= 3, command= lambda: rotate(dir,imgdir,num))
+rotation.place(relx=0.1, rely=0.8, anchor=N)
 
-trans = Button(main, text="Translate",font="Times 12",bd= 3, command= lambda: translate(dir,imgdir))
-trans.place(relx=0.5, rely=0.6, anchor=N)
+resizing = Button(main, text="Resize",font="Times 12",bd= 3, command= lambda: resize(dir,imgdir,num))
+resizing.place(relx=0.3, rely=0.8, anchor=N)
 
-rotation = Button(main, text="Rotation",font="Times 12",bd= 3, command= lambda: rotate(dir,imgdir))
-rotation.place(relx=0.1, rely=0.6, anchor=N)
+trans = Button(main, text="Translate",font="Times 12",bd= 3, command= lambda: translate(dir,imgdir,num))
+trans.place(relx=0.5, rely=0.8, anchor=N)
 
-flipping = Button(main, text="Flip",font="Times 12",bd= 3, command= lambda: flip(dir,imgdir))
-flipping.place(relx=0.7, rely=0.6, anchor=N)
+flipping = Button(main, text="Flip",font="Times 12",bd= 3, command= lambda: flip(dir,imgdir,num))
+flipping.place(relx=0.7, rely=0.8, anchor=N)
 
-zooming = Button(main, text="Zoom",font="Times 12",bd= 3, command= lambda: zoom(dir,imgdir))
-zooming.place(relx=0.9, rely=0.6, anchor=N)
+zooming = Button(main, text="Zoom",font="Times 12",bd= 3, command= lambda: zoom(dir,imgdir,num))
+zooming.place(relx=0.9, rely=0.8, anchor=N)
 
 
 main.mainloop()
--- milestone1.py
+++ milestone1.py
@@ -8,7 +8,8 @@ import os
 #Functions for Augmentations
 
 
-def resize(path,dir):
+def resize(path,dir,no):
+    num = int(no)
     plt.close()
     fig = plt.figure(figsize=(16, 8))
     imagePath = path
@@ -16,7 +17,7 @@ def resize(path,dir):
     (h,w) = image.shape[:2]
     aspect = w/h
     os.chdir(dir)
-    for i in range(15):
+    for i in range(num):
         ratio = random.uniform(0.2,(1080/h))
 
         height = int(ratio * h) 
@@ -26,65 +27,68 @@ def resize(path,dir):
 
         resizedImage = cv2.resize(image, dimension, interpolation=cv2.INTER_CUBIC)
         cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), resizedImage) 
-        fig.add_subplot(3, 5, i+1)
+        fig.add_subplot(4, (num//4)+1, i+1)
         plt.imshow(resizedImage)
 
     plt.show()
 
-def translate(path,dir):
+def translate(path,dir,no):
+    num = int(no)
     plt.close()
     fig = plt.figure(figsize=(16, 8))
     imagePath = path
     image = cv2.imread(imagePath)
     (h,w) = image.shape[:2]
     os.chdir(dir)
-    for i in range(15):
+    for i in range(num):
         translationMatrix = np.float32([[1,0, random.randint(10,(w//3.3))],[0,1,random.randint(10,(h//2.67))]])
         movedImage = cv2.warpAffine(image, translationMatrix, (image.shape[1],image.shape[0]))
         cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), movedImage)
-        fig.add_subplot(3, 5, i+1)
+        fig.add_subplot(4, (num//4)+1, i+1)
         plt.imshow(movedImage)
     plt.show()
     
-def rotate(path,dir):
+def rotate(path,dir,no):
+    num = int(no)
     plt.close()
     fig = plt.figure(figsize=(16, 8))
     imagePath = path
     image = cv2.imread(imagePath)
     center = tuple(np.array(image.shape[1::-1]) / 2)
     os.chdir(dir)
-    for i in range(15):
+    for i in range(num):
         angle = random.randint(-180,180)
         rotationMatrix = cv2.getRotationMatrix2D(center, angle, 1.0)
         rotatedImage = cv2.warpAffine(image, rotationMatrix, (image.shape[1],image.shape[0]))
         cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), rotatedImage)
-        fig.add_subplot(3, 5, i+1)
+        fig.add_subplot(4, (num//4)+1, i+1)
         plt.imshow(rotatedImage)
     plt.show()
 
-def flip(path,dir):
+def flip(path,dir,no):
+    num = int(no)
     plt.close()
     fig = plt.figure(figsize=(16, 8))
     imagePath = path
     image = cv2.imread(imagePath)
     os.chdir(dir)
-    for i in range(15):
+    for i in range(num):
         choice = random.randint(-1,1)
         flipped = cv2.flip(image, choice)
         cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), flipped)
-        fig.add_subplot(3, 5, i+1)
+        fig.add_subplot(4, (num//4)+1, i+1)
         plt.imshow(flipped)
     plt.show()
      
-def zoom(path,dir):
+def zoom(path,dir,no):
+    num = int(no)
     plt.close()
     fig = plt.figure(figsize=(16, 8))
     imagePath = path
     image = cv2.imread(imagePath)
-    center = tuple(np.array(image.shape[1::-1]) / 2)
     (h1,w1) = image.shape[:2]
     os.chdir(dir)
-    for i in range(15):
+    for i in range(num):
         zoom_factor = random.uniform(1.2,2)
         zoomed = cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor, interpolation=cv2.INTER_CUBIC)
         (h2,w2) = zoomed.shape[:2]
@@ -92,7 +96,7 @@ def zoom(path,dir):
         diff_w = (w2-w1)//2
         cropped = zoomed[ diff_h : h2 - diff_h , diff_w : w2 - diff_w]
         cv2.imwrite('New_Image{} .jpg'.format(random.randint(1,1000000000)), cropped)
-        fig.add_subplot(3, 5, i+1)
+        fig.add_subplot(4, (num//4)+1 , i+1)
         plt.imshow(cropped) 
     plt.show()
 
